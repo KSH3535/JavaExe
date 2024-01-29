3be840d9b1321975f939a0a774ffd9b660e7138b
KSH3535 <suhyeon5328@naver.com>
2024-01-29 10:48:30 +0900

    예금,출금 예제 (Integer.parseInt() 문자열 정수변환)

--- Ch03_Control/src/ch03/continue11/Resolve3.java
+++ Ch03_Control/src/ch03/continue11/Resolve3.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 public class Resolve3 {
 // while문과 Scanner의 nextLine() 메소드를 이용해서 다음 실행 결과와 같이
 // 키보드로부터 입력된 데이터로 예금, 출금, 조회, 종료 기능을 제공하는 코드를 작성하세요.
-	
-	public static void main(String[] args) {
+		
+	public static void method1() {
+		Scanner sc = new Scanner(System.in);
 		
 		int money = 0;
+		int input = 0;
+		boolean isRun = true;
 		
-		
-		while(true) {
-			
-			Scanner sc = new Scanner(System.in);
+		while(isRun) {
 			
 			System.out.println("----------------------------------");
 			System.out.println("1. 예금 | 2. 출금 | 3. 잔고 | 4. 종료");
@@ -21,48 +21,85 @@ public class Resolve3 {
 			System.out.print("선택>");
 			int choice = sc.nextInt();
 			
+			switch(choice) {
+				case 1 -> {
+					System.out.print("예금액>");
+					input = sc.nextInt();
+					money += input;
+					break;
+				}
+				case 2 -> {
+					System.out.print("출금액>");
+					input = sc.nextInt();
+					money -= input;
+					break;
+				}
+				case 3 -> {
+					System.out.printf("잔고>%d\n", money);
+					break;
+				}
+				case 4 -> {
+					isRun = false;
+					break;
+				}
+			}
 			
-			if (choice==4) {
-				System.out.println("프로그램 종료");
+		}
+		System.out.println("프로그램 종료");
+		
+		sc.close();
+	}
+	
+	
+	public static void method2() {
+		Scanner sc = new Scanner(System.in);
+		
+		int money = 0;
+		String input;
+		boolean isRun = true;
+		
+		while(isRun) {
+			System.out.println("----------------------------------");
+			System.out.println("1. 예금 | 2. 출금 | 3. 잔고 | 4. 종료");
+			System.out.println("----------------------------------");
+			System.out.print("선택>");
+			String choice = sc.nextLine();
+			
+			switch(choice) {
+			case "1" -> {
+				System.out.print("예금액>");
+				input = sc.nextLine();
+				money += Integer.parseInt(input);
+				// 교재 71p 참고
 				break;
 			}
-			else if (choice==1) {
-				System.out.print("예금액>");
-				int plus = sc.nextInt();
-				money += plus;
-			}
-			else if (choice==2) {
+			case "2" -> {
 				System.out.print("출금액>");
-				int minus = sc.nextInt();
-				money -= minus;
+				input = sc.nextLine();
+				money -= Integer.parseInt(input);
+				break;
 			}
-			else if (choice==3) {
+			case "3" -> {
 				System.out.printf("잔고>%d\n", money);
+				break;
 			}
-			
-			
-//			switch(choice) {
-//				case "1" -> {
-//					System.out.print("예금액>");
-//					int plus = sc.nextInt();
-//					money += plus;
-//				}
-//				case "2" -> {
-//					System.out.print("출금액>");
-//					int minus = sc.nextInt();
-//					money -= minus;
-//				}
-//				case "3" -> {
-//					System.out.printf("잔고>%d\n", money);
-//				}
-//				case "4" -> {System.out.println("프로그램 종료");}
-//			}
-			
-			sc.close();
+			case "4" -> {
+				isRun = false;
+				break;
+			}
+			}
+		
 		}
+		System.out.println("프로그램 종료");
 		
-		
-		
+		sc.close();
+	}
+	
+	
+	
+	public static void main(String[] args) {
+//		method1();
+		method2();
 	}
 
 }
