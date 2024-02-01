d4d327349023715224617d105b96555fe14be1bf
KSH3535 <suhyeon5328@naver.com>
2024-02-01 12:22:49 +0900

    Rectangle.java 예제 수정 전

--- Ch08_Class/src/ch08/class09/Rectangle.java
+++ Ch08_Class/src/ch08/class09/Rectangle.java
@@ -6,20 +6,18 @@ package ch08.class09;
 //int square() : 사각형 넓이 리턴
 //void show() : 좌표와 넓이 등 직사각형 정보의 화면 출력
 //boolean equals(Rectangle r) : 인자로 전달된 객체 r과 현 객체가 동일한 좌표의 직사각형이면 true 리턴
+
 public class Rectangle {
 	private int x1;
 	private int y1;
 	private int x2;
 	private int y2;
-	
-	public Rectangle(int x1,int y1,int x2,int y2) {
-		this.x1 = x1;
-		this.y1 = y1;
-		this.x2 = x2;
-		this.y2 = y2;
-	}
-	
+
 	public void set(int x1, int y1, int x2, int y2) {
+		if (x1 == x2 || y1 == y2) {
+			System.out.println("-----사각형이 아닙니다-----");
+//			return;
+		}
 		this.x1 = x1;
 		this.y1 = y1;
 		this.x2 = x2;
@@ -33,15 +31,20 @@ public class Rectangle {
 	}
 	
 	public void show() {
-		System.out.printf("입력한 좌표 : (%d, %d), (%d, %d)\n", x1, y1, x2, y2);
+		System.out.printf("입력한 좌표 : (%d, %d) (%d, %d)\n", x1, y1, x2, y2);
 		System.out.println("사각형의 넓이 : " + square());
+		System.out.println();
 	}
 	
 	public boolean equals(Rectangle r) {
-		return this.x1 == r.x1 &&
-				this.y1 == r.y1 &&
-				this.x2 == r.x2 &&
-				this.y2 == r.y2;
+		if ((this.x1 == r.x1 || this.x1==r.x2) &&
+				(this.x2 == r.x2 || this.x2==r.x1) &&
+				(this.y1 == r.y1 || this.y1==r.y2) &&
+				(this.y2 == r.y2 || this.y2==r.y1)) {
+				System.out.println("같은 사각형 입니다.");
+				return true;
+			} else
+				return false;
 	}
 	
 	
--- Ch08_Class/src/ch08/class09/RectangleMain.java
+++ Ch08_Class/src/ch08/class09/RectangleMain.java
@@ -2,7 +2,26 @@ package ch08.class09;
 
 public class RectangleMain {
 	public static void main(String[] args) {
+		Rectangle rec = new Rectangle();
+		rec.set(1, 3, 5, 8);
+		rec.show();
 		
 		
+		Rectangle rec1 = new Rectangle();
+		rec1.set(1, 3, 1, 8);
+		if(rec.equals(rec1)) {
+			System.out.println("두 좌표로 사각형의 넓이를 구할 수 없습니다.");
+		} else {
+			rec1.show();
+		}
+		
+		
+		Rectangle rec2 = new Rectangle();
+		rec2.set(1, 3, 5, 8);
+		if(rec.equals(rec2)) {
+			
+		} else {
+			rec2.show();
+		}
 	}
 }
