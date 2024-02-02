e1fb58f76de303cf4002650ebcfb109e87d5859c
KSH3535 <suhyeon5328@naver.com>
2024-02-02 16:02:58 +0900

    Car 예제 1. interface의 용법 (ICar를 상속받은 구현클래스 / ICar를 주입받은 테스트 클래스) 2. CarTest 구현 3. 제조사별 구현 클래스 4. CarMain에서 전체 통합 구현

---
+++ Ch09_Extends/src/ch09/interface13/factory01/FordCar.java
@@ -0,0 +1,27 @@
+package ch09.interface13.factory01;
+
+import ch09.interface13.ICar;
+
+public class FordCar implements ICar {
+
+	@Override
+	public void turnOn() throws InterruptedException {
+		System.out.println("FordCar - The engine runs smoothly~F");
+		Thread.sleep((long)(Math.random() * 500) + 500);	}
+
+	@Override
+	public void turnOff() throws InterruptedException {
+		System.out.println("FordCar - The engine turns off correctly~F");
+		Thread.sleep((long)(Math.random() * 500) + 500);	}
+
+	@Override
+	public void accel() throws InterruptedException {
+		System.out.println("FordCar - The accelerator accelerates well~F");
+		Thread.sleep((long)(Math.random() * 500) + 500);	}
+
+	@Override
+	public void footBreak() throws InterruptedException {
+		System.out.println("FordCar - The brakes operate correctly~F");
+		Thread.sleep((long)(Math.random() * 500) + 500);	}
+
+}
