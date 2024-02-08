a10fe811e520e7e35c5a9fb145800349d9f42b9d
KSH3535 <suhyeon5328@naver.com>
2024-02-08 18:13:50 +0900

    컬렉션 프레임워크 3. Map (HashMap)

---
+++ Ch10_Collection/src/ch10/collection02/hashmap04/EmployeeMain.java
@@ -0,0 +1,10 @@
+package ch10.collection02.hashmap04;
+
+public class EmployeeMain {
+	public static void main(String[] args) {
+		EmployeeManager em = new EmployeeManager();
+		em.run();
+		
+	}
+
+}
