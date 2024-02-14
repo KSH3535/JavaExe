2a08b51ba67def6875633b75efdc97178be7bfe6
KSH3535 <suhyeon5328@naver.com>
2024-02-14 17:39:49 +0900

    함수형 인터페이스 메서드 전달 생성자 전달

---
+++ Ch16_Lamda/src/ch16/lamda01/ch16/sec05/exam02/MethodReferenceExample.java
@@ -0,0 +1,12 @@
+package ch16.lamda01.ch16.sec05.exam02;
+
+public class MethodReferenceExample {
+
+	public static void main(String[] args) {
+		Person person = new Person();
+		
+		person.ordering(String::compareToIgnoreCase);
+		// person.ordering((a, b) -> a.compareToIgnoreCase(b));
+	}
+
+}
